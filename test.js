99a05c15044dc3acaf79b8fd4bc720b000ff17e7
Sourabh <sourabh.agarwal@flipkart.com>
2012-10-29 13:43:13 +0530

    making a dao layer

--- test.js
+++ test.js
@@ -5,11 +5,35 @@
 //var logger = log4js.getLogger('cheese');
 //logger.setLevel('INFO');
 
-var logger = require('./lib/LogFactory').create("test");
+//var logger = require('./lib/LogFactory').create("test");
+//
+//logger.trace('Entering cheese testing');
+//logger.debug('Got cheese.');
+//logger.info('Cheese is Gouda.');
+//logger.warn('Cheese is quite smelly.');
+//logger.error('Cheese is too ripe!');
+//logger.fatal('Cheese was breeding ground for listeria.');
 
-logger.trace('Entering cheese testing');
-logger.debug('Got cheese.');
-logger.info('Cheese is Gouda.');
-logger.warn('Cheese is quite smelly.');
-logger.error('Cheese is too ripe!');
-logger.fatal('Cheese was breeding ground for listeria.');
+var comb = require('comb');
+
+var Animal = comb.define(null, {
+	instance : {
+		constructor : function(options){
+			console.log(options);
+			console.log("Animal Created");
+		}
+	}
+});
+
+var Dog = comb.define(Animal, {
+	instance : {
+		constructor : function(options){
+			console.log("Dog Created");
+			options.color = "Black";
+			console.log(arguments);
+			this._super(arguments);
+		}
+	}
+});
+
+new Dog({name : "Tom"});
