1c017578a3eb9a129d225ef8951151c4a28eea1a
Sourabh <sourabh.agarwal@flipkart.com>
2012-10-31 01:19:46 +0530

    returning message when page can not be loaded

--- routes/w3/bench.js
+++ routes/w3/bench.js
@@ -27,21 +27,28 @@ var includeCss = function(url, window){
 exports.fetch = function(req, res){
 	var url = req.query.url;
 	jsdom.env(url, function(errors, window){
-		includeCss('/css/frame.css', window);
-		
-		var overlay = window.document.createElement("div");
-		overlay.id = "selected";
-		overlay.style = "position:absolute";
-		window.document.body.appendChild(overlay);
-		
-		includeJs('/lib/jquery-ui/js/jquery-1.8.2.js', window);
-		includeJs('/lib/jquery-ui/js/jquery-ui-1.9.0.custom.js', window);
-		includeJs('/javascripts/stack.js', window);
-		includeJs('/javascripts/movement.js', window);
-		includeJs('/javascripts/frame.js', window);
-		
-		//console.log(window.document.head.innerHTML);
-		
-		res.send(window.document.innerHTML);
+		if(window != undefined){
+			includeCss('/css/frame.css', window);
+			
+			var overlay = window.document.createElement("div");
+			overlay.id = "selected";
+			overlay.style = "position:absolute";
+			window.document.body.appendChild(overlay);
+			
+			includeJs('/lib/jquery-ui/js/jquery-1.8.2.js', window);
+			includeJs('/lib/jquery-ui/js/jquery-ui-1.9.0.custom.js', window);
+			includeJs('/javascripts/stack.js', window);
+			includeJs('/javascripts/movement.js', window);
+			includeJs('/javascripts/frame.js', window);
+			
+			//console.log(window.document.head.innerHTML);
+			
+			res.send(window.document.innerHTML);
+		}else{
+			/**
+			 * Could not load the page
+			 */
+			res.send("Sorry! Can not proceed. Try to put http:// before the URL if you didn't do it earlier");
+		}
 	});
 };
